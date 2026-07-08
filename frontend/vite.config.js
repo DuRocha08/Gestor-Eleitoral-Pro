4644830cad22ab68f87f3c7b57793062def6bb0f
DuRocha08 <287537192+DuRocha08@users.noreply.github.com>
2026-07-08 12:56:21 -0300

    atualizada funcionando

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const BASE_GITHUB_PAGES = '/GESTOR-ELEITORAL/';
+const BASE_GITHUB_PAGES = '/Gestor-Eleitoral-Pro/';
 
 function pluginConfigPublica(urlApi) {
   return {
